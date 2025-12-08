31ab146b66dc6a8b3c9203a5037103ca57e81116
NuclearRoller <youyou.aliz@yahoo.com>
2025-12-08 00:05:18 +0000

    added Mahmoud branch

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -11,7 +11,7 @@ const branchNamesArabic = {
   Nahia: "ناهيا",
   Faisal: "فيصل",
   Alaa: "الاء",
- Mahmoud: "محمود",
+Mahmoud: "محمود",
 };
 
 export default function Leaderboard({ monthAverage, colors }) {
